614752e9a749974dac3cc969397889e77bc62656
Dan Mathew <83295199+danbmathew@users.noreply.github.com>
2023-08-22 00:27:33 +0530

    Update Check_status.py

--- Check_status.py
+++ Check_status.py
@@ -2,6 +2,7 @@ import ssl
 import socket
 import datetime
 import requests
+import argparse
 
 def certificate_details(host):
     connection = context.wrap_socket(socket.socket(socket.AF_INET), server_hostname = host)
@@ -17,16 +18,12 @@ def get_expiry_date(host):
 
     return expiry_date
 
-#Creating an SSL Context
+
 context = ssl.create_default_context()
 
-hosts = open('hostnames.txt')
-slack_webhook = 'https://hooks.slack.com/services/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'
+parser = argparse.ArgumentParser()
+parser.add_argument("host")
+arg = parser.parse_args()
 
-
-for each in hosts:
-    host = each.rstrip()
-    expiry_date = get_expiry_date(host)
-    remaining_days_to_expire = (expiry_date - datetime.datetime.now()).days
-    message = '* Domain : {} \n * Warning : The SSL certificate for {} will expire in {} days.'.format(host, host, remaining_days_to_expire)
-    requests.post(slack_webhook, json={'text': message})
+expiry_date = get_expiry_date(arg.host)
+remaining_days_to_expire = (expiry_date - datetime.datetime.now()).days
